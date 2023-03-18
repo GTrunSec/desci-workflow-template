61175802c0b584b987831ecb91e0555eb5a1a59d
guangtao <gtrunsec@hardenedlinux.org>
2023-03-18 01:19:17 -0700

    Push full image

--- nix/kernels/devshellProfiles.nix
+++ nix/kernels/devshellProfiles.nix
@@ -10,14 +10,16 @@ in {
       {
         name = "jupyenv";
         command = ''
-          nix run $PRJ_ROOT#${nixpkgs.system}.kernels.entrypoints.jupyenv "$@"
+          ${cell.packages.jupyenv.config.build}/bin/jupyter "$@"
+          # nix run $PRJ_ROOT#${nixpkgs.system}.kernels.entrypoints.jupyenv "$@"
         '';
         help = "Run wrapped jupyenv";
       }
       {
         name = "quarto";
         command = ''
-          nix run $PRJ_ROOT#${nixpkgs.system}.kernels.packages.jupyenv.config.quartoEnv "$@"
+          ${l.getExe cell.packages.jupyenv.config.quartoEnv} "$@"
+          # nix run $PRJ_ROOT#${nixpkgs.system}.kernels.packages.jupyenv.config.quartoEnv "$@"
         '';
         help = "Run wrapped quarto";
       }
